6b46ac1728d632546f448b0dfbc0b11af432502e
Towseen7 <akinadeaj@gmail.com>
2022-07-01 17:17:15 -0700

    commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-nclude <stdio.h>
+include <stdio.h>
 /**
- *main - Entry point
- *my hello, printf task
- *Return: return 1 after program execution 
+ * main - Entry point
+ * my hello, printf task
+ * Return: return 1 after program execution 
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
+return (0); }
