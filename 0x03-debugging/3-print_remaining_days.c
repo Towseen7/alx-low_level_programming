e207396aed68ea4914bb5dc1f5bc576b82d1f368
Towseen7 <akinadeaj@gmail.com>
2022-07-07 15:36:02 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,6 +18,11 @@ if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month > 2 && day >= 60)
 {
+day++;
+}
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
 else
 {
 if (month == 2 && day == 60)
