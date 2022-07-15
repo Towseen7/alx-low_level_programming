250064ffb4f80ca097f8c9dcc09a1d192850193f
Towseen7 <akinadeaj@gmail.com>
2022-07-14 20:48:17 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -32,6 +32,7 @@ int check_seperators(char c)
 {
 	int i = 0;
 	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
 	for (; i < 13; i++)
 	{
 		if (c == seperators[i])
