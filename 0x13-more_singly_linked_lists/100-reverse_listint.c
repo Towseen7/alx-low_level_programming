4d538a00263e1cc35d1f9c551fdb457d270d2247
Towseen7 <akinadeaj@gmail.com>
2022-08-16 20:09:42 -0700

    task11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,7 +10,7 @@
 listint_t *reverse_listint(listint_t **head)
 {
 listint_t *ahead, *behind;
-if (*head == NULL || *head == NULL)
+if (head == NULL || *head == NULL)
 return (NULL);
 behind = NULL;
 while ((*head)->next != NULL)
@@ -18,7 +18,7 @@ while ((*head)->next != NULL)
 ahead = (*head)->next;
 (*head)->next = behind;
 behind = *head;
-*head = *ahead;
+*head = ahead;
 }
 (*head)->next = behind;
 return (*head);
