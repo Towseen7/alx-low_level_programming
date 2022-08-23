e5b707e378954a2208ef27b0466d158b1f1adb67
Towseen7 <akinadeaj@gmail.com>
2022-08-23 00:10:18 -0700

    task100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -236,14 +236,14 @@ exit(98);
 }
 /**
 * main - Displays the information contained in the
-*        ELF header at the start of an ELF file.
+* ELF header at the start of an ELF file.
 * @argc: The number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments.
 *
 * Return: 0 on success.
 *
 * Description: If the file is not an ELF File or
-*              the function fails - exit code 98. 
+* the function fails - exit code 98.
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
@@ -263,7 +263,7 @@ dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
 }
 r = read(o, header, sizeof(Elf64_Ehdr));
-f (r == -1)
+f(r == -1)
 {
 free(header);
 close_elf(o);
